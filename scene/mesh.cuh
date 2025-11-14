e7abd8085358693901005a5d430220eb383ff378
Sam Hill <smotired@gmail.com>
2025-11-13 21:18:25 -0700

    Proper destructor for mesh file

--- scene/mesh.cuh
+++ scene/mesh.cuh
@@ -19,8 +19,27 @@ protected:
 
     Box box;    // outer bounding box
 
-    void Clear() { nv = 0; nf = 0; nvn = 0; nvt = 0; box.Init(); }
+    void Clear() {
+        nv = 0;
+        nf = 0;
+        nvn = 0;
+        nvt = 0;
+        box.Init();
+
+        cudaFree(v);
+        cudaFree(f);
+        cudaFree(vn);
+        cudaFree(fn);
+        cudaFree(vt);
+        cudaFree(ft);
+    }
 public:
+    Mesh() : v(nullptr), f(nullptr), vn(nullptr), fn(nullptr), vt(nullptr), ft(nullptr),
+            nv(0), nf(0), nvn(0), nvt(0), box() { }
+    Mesh( Mesh const &t ) : v(nullptr), f(nullptr), vn(nullptr), fn(nullptr), vt(nullptr), ft(nullptr),
+            nv(0), nf(0), nvn(0), nvt(0), box() { *this = t; }
+    virtual ~Mesh() { Clear(); }
+
     [[nodiscard]] __device__ bool HasNormals() const { return nvn > 0; }
     [[nodiscard]] __device__ bool HasTextureVertices() const { return nvt > 0; }
 
